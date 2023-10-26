71cd5402dd38bea9d1d3cdd6e833a885f3e9beec
spark2 <spark2@cx2r3s4.42seoul.kr>
2023-10-26 22:17:20 +0900

    [Chore] pipe, heredoc 추가 및 main 로직 구현

--- spark2/main.c
+++ spark2/main.c
@@ -6,7 +6,7 @@
 /*   By: spark2 <spark2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 18:54:38 by spark2            #+#    #+#             */
-/*   Updated: 2023/10/18 21:07:51 by spark2           ###   ########.fr       */
+/*   Updated: 2023/10/26 21:50:17 by spark2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,36 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	t_arg	arg;
+	t_cmd	cmd;
 	char	*line;
 	char	**temp;
 
 	(void)argc;
 	(void)argv;
+	ft_memset(&arg, 0, sizeof(t_arg));
+	set_file(&arg, argc, argv);
+	if (arg.here_flag)
+		run_heredoc(&arg, argv[2]);
+	get_path_envp(&arg, envp);
+	set_cmd(&arg, &cmd, argc, argv);
+	run_fork(&arg, &cmd, argc, envp);
+	run_free(&arg, &cmd, argc);
+	unlink("/tmp/.infile");
+
+	dup2(0, STDIN_FILENO);
+	dup2(1, STDOUT_FILENO);
+	(void)envp;
 	while (1)
 	{
 		line = readline("minishell $ ");
 		if (!line)
 			break ;
+		printf("here\n");
 		if (*line)
 			add_history(line);
 		temp = ft_split(line, ' ');
-		check_builtins(temp, envp);
+		check_builtins(&arg, temp, envp);
 		free(line);
 		free(temp);
 	}
--- spark2/minishell.h
+++ spark2/minishell.h
@@ -6,7 +6,7 @@
 /*   By: spark2 <spark2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 21:01:32 by spark2            #+#    #+#             */
-/*   Updated: 2023/10/20 18:14:04 by spark2           ###   ########.fr       */
+/*   Updated: 2023/10/26 21:29:18 by spark2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,66 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <signal.h>
+# include <fcntl.h>
 
 # include <readline/readline.h>
 # include <readline/history.h>
 
 # include "./libft/libft.h"
+# include "./pipex/get_next_line/get_next_line.h"
 
-void	check_builtins(char **line, char **envp);
+typedef struct s_list_ms
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list_ms;
+
+typedef struct s_cmd_arg
+{
+	char	**arr;
+}	t_cmd_arg;
+
+typedef struct s_cmd
+{
+	t_cmd_arg	*arg;
+}	t_cmd;
+
+typedef struct s_arg
+{
+	int		infile;
+	int		outfile;
+	char	**path;
+	int		pipe_fd[2];
+	int		here_flag;
+	pid_t	pid;
+}	t_arg;
+
+void	check_builtins(t_arg *arg, char **line, char **envp);
 void	ft_error(char *str);
+void	run_heredoc(t_arg *arg, char *limiter);
+
+//pipex
+void	check_argc(int argc);
+void	set_file(t_arg *arg, int argc, char **argv);
+
+void	run_heredoc(t_arg *arg, char *limiter);
+
+void	get_path_envp(t_arg *arg, char **envp);
+char	*get_cmd_path(char **path, char *cmd);
+
+void	set_cmd(t_arg *arg, t_cmd *cmd, int argc, char **argv);
+
+void	run_fork(t_arg *arg, t_cmd *cmd, int argc, char **envp);
+
+void	run_free(t_arg *arg, t_cmd *cmd, int argc);
+void	free_2d_array(char **str);
+void	print_error(char *err_msg);
+
+void	infile_to_pipe(t_arg *arg);
+void	pipe_to_pipe(t_arg *arg);
+void	pipe_to_outfile(t_arg *arg);
+void	parent_work(t_arg *arg);
+
+void	wait_process(void);
 
 #endif
