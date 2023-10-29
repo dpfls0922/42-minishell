eaea24dcaa8780326dd3bed2499f5947e9d3a9bd
dpfls0922 <dpfls0922@naver.com>
2023-10-29 19:35:39 +0900

    [Fix] free가 이미 된 상황이어서 쓰레기값을 참조하는 에러 해결

--- yerilee/delete_quotes.c
+++ yerilee/delete_quotes.c
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 18:52:16 by yerilee           #+#    #+#             */
-/*   Updated: 2023/10/29 19:04:54 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/10/29 19:33:18 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,6 @@ char	*delete_quotes_in_str(char *str)
 	{
 		old = ft_substr(str, 0, ft_strlen(str));
 		new = ft_delete_quotes(old, 0, 0, 0);
-		free(str);
 		str = ft_substr(new, 0, ft_strlen(new));
 		free(old);
 		free(new);
