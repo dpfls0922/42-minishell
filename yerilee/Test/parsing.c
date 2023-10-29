aecfa40436382670d15222e554009f667f8b7315
dpfls0922 <dpfls0922@naver.com>
2023-10-29 20:52:07 +0900

    [Test] exit_status 관련 함수 틀 짜기

--- yerilee/Test/parsing.c
+++ yerilee/Test/parsing.c
@@ -6,19 +6,18 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:27:26 by yerilee           #+#    #+#             */
-/*   Updated: 2023/10/29 18:46:14 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/10/29 20:32:36 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
 
-/* $? 처리 -> heredoc 처리 -> command_list 만들기 -> quotes 제거 */
 void	parsing(t_data *data)
 {
 	t_lexer	*lexer;
 
 	lexer = data->lexer_list;
-	// $? 처리
+	// handle_exit_status(data);
 	handle_heredoc(data, lexer);
 	handle_command(data);
 	delete_quotes_in_list(data->cmd_list);
