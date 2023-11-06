95c54267a801cfec9519d3e80dd4f791b0700aec
dpfls0922 <dpfls0922@naver.com>
2023-11-06 20:31:30 +0900

    [Test] exit status 처리

--- yerilee/Test/exit_status.c
+++ yerilee/Test/exit_status.c
@@ -6,16 +6,49 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 20:10:04 by yerilee           #+#    #+#             */
-/*   Updated: 2023/10/29 20:51:11 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/11/06 16:45:49 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
 
+char	*allocate_new_val(char *prev_exit, char *value)
+{
+	int		len;
+	char	*new;
+
+	len = ft_strlen(prev_exit) + ft_strlen(value) - 2;
+	new = malloc(sizeof(char) * (len + 1));
+	if (!new)
+		return (NULL);
+	return (new);
+}
 
 char	*change_exit_status(t_data *data, char *value)
 {
-	return (value);
+	t_exit	exit;
+
+	init_exit(data, &exit, value);
+	while (exit.value[exit.i])
+	{
+		if (exit.value[exit.i] == '\"' && exit.s_flag == 0)
+			exit.d_flag = !exit.d_flag;
+		if (exit.value[exit.i] == '\'' && exit.d_flag == 0)
+			exit.s_flag = !exit.s_flag;
+		if (exit.s_flag == 0 && exit.value[exit.i] == '$'
+			&& exit.value[exit.i + 1] == '?')
+		{
+			exit.new_val[exit.j] = '\0';
+			exit.new_val = ft_strjoin(exit.new_val, exit.prev_exit);
+			exit.j += ft_strlen(exit.prev_exit);
+			exit.i += 2;
+		}
+		else
+			exit.new_val[exit.j++] = exit.value[exit.i++];
+	}
+	free(exit.prev_exit);
+	exit.new_val[exit.j] = '\0';
+	return (exit.new_val);
 }
 
 void	handle_exit_status(t_data *data)
