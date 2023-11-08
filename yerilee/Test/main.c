42be434365bdb04026c737d130f15f62a3cf8ae1
dpfls0922 <dpfls0922@naver.com>
2023-11-08 17:22:49 +0900

    [Test] 사용을 완료한 후 연결 리스트 동적할당 해제

--- yerilee/Test/main.c
+++ yerilee/Test/main.c
@@ -6,11 +6,21 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:34:27 by yerilee           #+#    #+#             */
-/*   Updated: 2023/11/06 17:49:51 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/11/08 17:00:21 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_free_data(t_data *data)
+{
+	if (data->heredoc_num)
+		ft_free_list(data->end);
+	if (data->lexer_list)
+		ft_free_lexer(data->lexer_list);
+	if (data->cmd_list)
+		ft_free_cmd(data->cmd_list);
+}
 
 int	minishell(t_data *data)
 {
@@ -36,11 +46,12 @@ int	minishell(t_data *data)
 			expanding(data);
 			parsing(data);
 			// execution
+			ft_free_data(data);
 		}
 		data->prev_exit_status = g_vars.exit_status;
-		rl_clear_history();
-		// free data
 	}
+	rl_clear_history();
+	ft_free_env(data->env_list);
 	return (0);
 }
 
