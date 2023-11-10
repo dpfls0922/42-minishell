fb5c0ead2cabd80461bde5c587bfdc392e07dd45
spark2 <spark2@c3r2s2.42seoul.kr>
2023-11-10 23:39:07 +0900

    [Chore] 전체 로직 테스트 진행 중

--- spark2/main.c
+++ spark2/main.c
@@ -6,7 +6,7 @@
 /*   By: spark2 <spark2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 18:54:38 by spark2            #+#    #+#             */
-/*   Updated: 2023/11/09 19:55:06 by spark2           ###   ########.fr       */
+/*   Updated: 2023/11/10 23:05:05 by spark2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,33 @@ int	main(int argc, char **argv, char **envp)
 	// set_cmd(&arg, &cmd, argc, argv);
 	// run_free(&arg, &cmd, argc);
 	// unlink("/tmp/.infile");
-	
-	while (1)
-	{
-		data.cmd = readline("minishell $ ");
-		if (!data.cmd)
-			break ;
-		else if (!*data.cmd)
-			free(data.cmd);
-		if (data.cmd)
-			add_history(data.cmd);
-		data.cmd_list->cmd = ft_split(data.cmd, ' ');
-		if (data.heredoc_num) //합칠 때 if (cmd.heredoc_num) 으로 수정
-			run_heredoc(&data, data.end[0]); //합칠 때 limiter 매개변수 수정
-		exec_start(data.cmd_list->cmd, &data);
-		free(data.cmd);
-		free(data.cmd_list->cmd);
-	}
+
+	int	i;
+	i = 1;
+	t_cmd *tmp;
+	tmp = data.cmd_list;
+	tmp->cmd = ft_split(argv[1], ' ');
+	tmp->next->cmd = ft_split(argv[2], ' ');
+	exec_start(data.cmd_list->cmd, &data);
+
+	//real code
+	// while (1)
+	// {
+	// 	data.cmd = readline("minishell $ ");
+	// 	if (!data.cmd)
+	// 		break ;
+	// 	else if (!*data.cmd)
+	// 		free(data.cmd);
+	// 	if (data.cmd)
+	// 		add_history(data.cmd);
+	// 	data.cmd_list->cmd = ft_split(data.cmd, ' ');
+	// 	if (data.heredoc_num) //합칠 때 if (cmd.heredoc_num) 으로 수정
+	// 		run_heredoc(&data, data.end[0]); //합칠 때 limiter 매개변수 수정
+	// 	exec_start(data.cmd_list->cmd, &data);
+	// 	free(data.cmd);
+	// 	data.cmd = 0;
+	// 	free(data.cmd_list->cmd);
+	// 	data.cmd_list->cmd = 0;
+	// }
 	return (0);
 }
