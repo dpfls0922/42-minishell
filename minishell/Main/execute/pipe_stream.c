819f4a5f6842c4c9065a08e24c17e8ba002bacc4
dpfls0922 <dpfls0922@naver.com>
2023-11-17 20:26:32 +0900

    [Chore] 필요없는 출력문 삭제

--- minishell/Main/execute/pipe_stream.c
+++ minishell/Main/execute/pipe_stream.c
@@ -6,7 +6,7 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:30:44 by spark2            #+#    #+#             */
-/*   Updated: 2023/11/17 19:54:17 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/11/17 20:26:09 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	infile_to_pipe(t_cmd *cmd)
 {
 	if (cmd->fd_in < 0)
 		print_error("fd_in error");
-	write(1, "infile to pipe\n", 15);
 	close(cmd->pipe_fd[0]);
 	dup2(cmd->fd_in, STDIN_FILENO);
 	dup2(cmd->pipe_fd[1], STDOUT_FILENO);
@@ -30,12 +29,10 @@ void	pipe_to_pipe(t_cmd *cmd)
 	close(cmd->pipe_fd[0]);
 	dup2(cmd->pipe_fd[1], STDOUT_FILENO);
 	close(cmd->pipe_fd[1]);
-	write(1, "pipe to pipe\n", 13);
 }
 
 void	pipe_to_outfile(t_cmd *cmd)
 {
-	write(1, "pipe to outfile\n", 16);
 	close(cmd->pipe_fd[1]);
 	dup2(cmd->fd_out, STDOUT_FILENO);
 	if (cmd->fd_out != 1)
