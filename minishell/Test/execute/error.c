976c62a4c35bd95074bd77af63cda7c2dc599cb4
Newsujin <chemd78@naver.com>
2023-11-21 22:06:04 +0900

    [Feat] cmd_not_found error 함수 생성

--- minishell/Test/execute/error.c
+++ minishell/Test/execute/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: spark2 <spark2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 18:13:20 by spark2            #+#    #+#             */
-/*   Updated: 2023/11/15 16:26:41 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/11/21 21:36:41 by spark2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,11 @@ void	print_error(char *err_msg)
 	write(2, err_msg, ft_strlen(err_msg));
 	unlink("/tmp/.infile");
 	exit(1);
+}
+
+void	cmd_not_found_error(char *cmd)
+{
+	printf("%s: command not found\n", cmd);
+	unlink("/tmp/.infile");
+	exit(127);
 }
