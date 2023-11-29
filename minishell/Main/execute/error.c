721b64f31d54728925b39ac3d7ae720d183e1770
dpfls0922 <dpfls0922@naver.com>
2023-11-29 21:48:43 +0900

    [Fix] heredoc과 존재하지 않는 명령어가 파이프로 연결되어 있을 때 발생하는 에러 해결

--- minishell/Main/execute/error.c
+++ minishell/Main/execute/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: spark2 <spark2@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 20:49:12 by spark2            #+#    #+#             */
-/*   Updated: 2023/11/27 20:49:13 by spark2           ###   ########.fr       */
+/*   Updated: 2023/11/29 21:40:01 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@ void	print_error(char *err_msg)
 {
 	write(2, err_msg, ft_strlen(err_msg));
 	unlink("/tmp/.infile");
-	exit(127);
+	exit(1);
 }
 
 void	cmd_not_found_error(char *cmd)
 {
-	printf("%s: command not found\n", cmd);
+	write(2, cmd, ft_strlen(cmd));
+	write(2, " : command not found\n", 22);
 	unlink("/tmp/.infile");
 	exit(127);
 }
