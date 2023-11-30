1f39de0b3b44ee6fc102f21fb9eae34516c42c44
Newsujin <chemd78@naver.com>
2023-11-30 19:03:46 +0900

    [Fix] 각 파이프의 fd_out이 지정이 되어 있을 경우 해당 fd_out으로 dup2

--- minishell/Main/execute/pipe_stream.c
+++ minishell/Main/execute/pipe_stream.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_stream.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: spark2 <spark2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:30:44 by spark2            #+#    #+#             */
-/*   Updated: 2023/11/29 20:49:22 by yerilee          ###   ########.fr       */
+/*   Updated: 2023/11/30 18:58:15 by spark2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,38 @@ void	infile_to_pipe(t_cmd *cmd)
 		exit(1);
 	}
 	close(cmd->pipe_fd[0]);
-	dup2(cmd->pipe_fd[1], STDOUT_FILENO);
+	if (cmd->fd_out != 1)
+	{
+		dup2(cmd->fd_out, STDOUT_FILENO);
+		close(cmd->fd_out);
+	}
+	else
+	{
+		dup2(cmd->pipe_fd[1], STDOUT_FILENO);
+		close(cmd->pipe_fd[1]);
+	}
 	if (cmd->fd_in != 0)
 		close(cmd->fd_in);
-	close(cmd->pipe_fd[1]);
 }
 
 void	pipe_to_pipe(t_cmd *cmd)
 {
 	close(cmd->pipe_fd[0]);
-	dup2(cmd->pipe_fd[1], STDOUT_FILENO);
-	close(cmd->pipe_fd[1]);
+	if (cmd->heredoc_num)
+	{
+		dup2(cmd->fd_in, STDIN_FILENO);
+		close(cmd->fd_in);
+	}
+	if (cmd->fd_out != 1)
+	{
+		dup2(cmd->fd_out, STDOUT_FILENO);
+		close(cmd->fd_out);
+	}
+	else
+	{
+		dup2(cmd->pipe_fd[1], STDOUT_FILENO);
+		close(cmd->pipe_fd[1]);
+	}
 }
 
 void	pipe_to_outfile(t_cmd *cmd)
